c591a3247a64b2cdeabb9f64082211daa019dbeb
dannypsnl <dannypsnl@gmail.com>
2020-04-15 18:42:36 +0800

    parse struct def outer part
    * provide keyword/p
    TODO
    * parse field
    * provide struct-def structure

--- c-parser.rkt
+++ c-parser.rkt
@@ -9,6 +9,10 @@
   ;;; lexeme would take at least one space or do nothing
   (do (or/p (many+/p space/p) void/p)
       (pure (lambda () 'lexeme))))
+(define (keyword/p keyword)
+  (do (string/p keyword)
+      (lexeme/p)
+      (pure keyword)))
 (define identifier/p
   (do [id <- (many/p letter/p)]
       (lexeme/p)
@@ -21,6 +25,14 @@
       (char/p #\;)
       (pure (CGlobalVarDef typ name))))
 
+(define struct-def/p
+  (do (keyword/p "struct")
+      [name <- identifier/p]
+      (char/p #\{)
+      ;;; TODO: parse field
+      (char/p #\})
+      (pure 'struct)))
+
 (module+ test
   (require rackunit)
   (require megaparsack megaparsack/text)
@@ -28,7 +40,10 @@
   (define (t-parse parser input-string)
     (parse-result! (parse-string parser input-string)))
 
+  (check-equal? (t-parse (keyword/p "abc") "abc") "abc")
   (check-equal? (t-parse identifier/p "a") "a")
   (check-equal? (t-parse global-var-def/p "int i;")
     (CGlobalVarDef "int" "i"))
+  (check-equal? (t-parse struct-def/p "struct Foo {}")
+    'struct)
   )
