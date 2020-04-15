ffb074450ad88bedd1d4bec49cc675722937e075
dannypsnl <dannypsnl@gmail.com>
2020-04-15 19:36:23 +0800

    parse one field correcly

--- c-parser.rkt
+++ c-parser.rkt
@@ -26,13 +26,13 @@
       (pure (CGlobalVarDef typ name))))
 
 (define struct-field/p
-  (list/p type/p identifier/p))
+  (list/p type/p identifier/p (char/p #\;)))
 (define struct-def/p
   (do (keyword/p "struct")
       [name <- identifier/p]
       (char/p #\{)
       (lexeme/p)
-      [fields <- (many/p struct-field/p #:sep (char/p #\;))]
+      [fields <- (many/p struct-field/p )]
       (lexeme/p)
       (char/p #\})
       (pure (CStructDef name fields))))
@@ -55,7 +55,7 @@
     (success
       (CStructDef "Foo"
         (list
-          (list "int" "i")))))
+          (list "int" "i" #\;)))))
   (check-equal? (t-parse struct-def/p "struct Foo { int i; int j; }")
     (success
       (CStructDef "Foo"
