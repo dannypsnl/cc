7d57b33a2c1d8914c5c039a5f4278773ec59b841
Lîm Tsú-thuàn≡悃悃 <dannypsnl@gmail.com>
2021-05-13 16:51:37 +0800

    revert

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define deps '("base"
                "reporter"
                "megaparsack-lib"
-               "functional"))
+               "functional-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/cc.scrbl" ())))
 (define pkg-desc "A compiler of C subset")
